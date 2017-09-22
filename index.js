c9da71d62b0ac9616100eea58781803f2bddbe36
maudetes <maudet.estelle@gmail.com>
2017-09-22 13:43:22 +0200

    add everything

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+app.use(express.static('.'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
