837b5b0e9ee1b220df15bbee7ddfde885e8342c4
maudetes <maudet.estelle@gmail.com>
2017-10-01 21:02:34 +0200

    create basic room system

--- index.js
+++ index.js
@@ -9,13 +9,32 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var numUsers = 0;
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  ++numUsers; 
+
+  if (numUsers % 2){ //odd -> create new room
+  	socket.room ='room'+ ((numUsers + 1) / 2);
+  } 
+  else {
+	socket.room ='room' + (numUsers/2);
+	// notify the user that he is user 2
+	socket.broadcast.to(socket.room).emit('player2');
+  }
+
+  socket.join(socket.room)
+
+  console.log('a user connected: '+numUsers+' user(s), room: '+socket.room);
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', function(){
+  	--numUsers;
+    console.log('user disconnected: '+numUsers+' left');
   });
 });
 
-http.listen(port, function(){
+expressServer = http.listen(port, function(){
   console.log('listening on *:' + port);
 });
