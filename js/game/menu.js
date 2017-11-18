dca4a0bce747b589aabac80de16bdeca0bfa8550
CamilleQuenin <camille.quenin@gmail.com>
2017-11-18 11:18:31 +0100

    change menu to start with scene1 instead of scene2

--- js/game/menu.js
+++ js/game/menu.js
@@ -58,12 +58,12 @@ function Launch_Game(){
 
 	Wait(500);
 	Clear();
-	//Start_Scene_1();
+	Start_Scene_1();
 
-	if(player == 1)
-		Start_Scene_2F();
-	else
-		Start_Scene_2A();
+	//if(player == 1)
+	//	Start_Scene_2F();
+	//else
+	//	Start_Scene_2A();
 }
 
 function Credits(message){
