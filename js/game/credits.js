b3bcbaa66551ffea3785a4b9597e68dc593022d4
Camille Quenin <camille.quenin@gmail.com>
2017-12-19 10:39:01 +0100

    add the credits (except images !)

--- js/game/credits.js
+++ js/game/credits.js
@@ -1,8 +1,41 @@
 // SCENE_4 : CREDITS
 
 function Credits(){
-	N("On n'a pas créé ce jeu à partir de rien.");
-	N("On s'est en premier basé sur le jeu Coming Out Simulator de Nick Cage.");
+	N("Ce jeu n'a pas été créé à partir de rien.");
+	N("Pour commencer, nous nous sommes inspiré du jeu Coming Out Simulator de Nicky Case.");
 	N("Essayez-le, ça vaut le détour.");
-	N("...");
+	N("http://ncase.me/cos/ (Public Domain)");
+	N("Nous avons aussi bénéficié des nombreux conceils de nos professeurs à l'UTC : Richard Cornu et Nicolas Esposito.");
+	N("Merci à eux.");
+
+	//SOUNDS SFX
+	N("Nous avons également utilisé des sons libres de droit trouvés sur les sites Freesound et SoundBible.");
+	N("En voici la liste :");
+	N("runnning on concrete.aif de MAJ061785 disponible sur https://freesound.org/s/85560/ sous la CC Attribution license (modifié).");
+	N("Police siren.mp3 de MultiMax2121 disponible sur https://freesound.org/s/156869/ sous la CC0 license (modifié).");
+	N("Door Open Close de amholma disponible sur https://freesound.org/s/344360/ sous la CC0 license (modifié).");
+	N("Knock_the_door.wav de anagar disponible sur https://freesound.org/s/267931/ sous la CC0 license (modifié).");
+	N("Heartbeats 61.wav de patobottos disponible sur https://freesound.org/s/369017/ sous la CC0 license (modifié).");
+	N("Sigh #2.MP3 de SoundsForHim disponible sur https://freesound.org/s/399641/ sous la CC0 license (modifié).");
+	N("Metal Knock 1.wav de dster777 disponible sur https://freesound.org/s/344565/ sous la CC0 license (modifié).");
+	N("Indoor adult murmur, small group.wav de SpliceSound disponible sur https://freesound.org/s/260121/ sous la CC0 license (modifié).");
+	N("breathJump02[THM_Adventure] de alex_audio disponible sur https://freesound.org/s/188541/ sous la CC Attribution license (modifié).");
+	N("CrazyLaugh.wav de engreitz disponible sur https://freesound.org/s/79769/ sous la CC Sampling+ license (modifié).");
+	N("Slam door.MP3 de SoundsForHim disponible sur https://freesound.org/s/79769/ sous la CC0 license (modifié).");
+	N("Car door close, engine start de FedeFrede disponible sur https://freesound.org/s/324138/ sous la CC Attribution license (modifié).");
+	N("40 Smith Wesson de Mike Koenig disponible sur http://soundbible.com/2122-40-Smith-Wesson.html sous la CC Attribution license (modifié).");
+
+	//SOUNDS LOOPS
+	N("De même, nous avons utilisé des musiques libres de droit trouvées sur le site Free Music Archive.");
+	N("En voici la liste :");
+	N("MO 30-1 CUT de Morgan Fisher disponible sur http://freemusicarchive.org/music/Morgan_Fisher/VA_-_Clinical_Jazz_excerpt_1/MO_30-1_CUT sous la CC Attribution-NonCommercial-NoDerivatives (aka Music Sharing) 3.0 International License.");
+	N("Nothing (Bonus Track) de Kai Engel disponible sur http://freemusicarchive.org/music/Kai_Engel/Chapter_One__Cold/Kai_Engel_-_Chapter_One_-_Cold_-_09_Nothing_Bonus_Track sous la CC Attribution license.");
+	N("Ambient-M (2003) de Antony Raijekov disponible sur http://freemusicarchive.org/music/Antony_Raijekov/Jazz_U/Ambient-M_2003 sous la CC Attribution-NonCommercial-ShareAlike license.");
+
+	//IMAGES
+	N("Enfin, nous avons utilisé des images issues du site Freepix.");
+	N("En voici la liste :");
+	N("");
+	N("");
+	N("");
 }
